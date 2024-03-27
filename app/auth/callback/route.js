99abcaa178b9b554b88b90cedc1affad3a5c4992
IsraelChidera <israelchidera54@gmail.com>
2024-03-27 01:35:49 +0100

    Modified: Route handler

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -5,13 +5,13 @@ import { NextResponse } from "next/server";
 export async function GET(request){
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get('code');
-
-    console.log("middleware code:", code);
+    
     if(code) {
         const cookieStore = cookies();
         const supabase = createRouteHandlerClient({cookie: () => cookieStore});
         await supabase.auth.exchangeCodeForSession(code);
     }
 
-    return NextResponse.redirect(requestUrl.origin);
+    // return NextResponse.redirect(requestUrl.origin);
+    return NextResponse.redirect(new URL('/dashboard', request.url))
 }
