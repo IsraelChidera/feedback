13d1473b46d9f6605eae525bf38617b34867efa9
IsraelChidera <israelchidera54@gmail.com>
2024-04-22 18:13:36 +0100

    user profile

--- components/widget/landing-page/Hero.tsx
+++ components/widget/landing-page/Hero.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+'use client'
+import React, { useContext } from 'react';
 import Navbar from './Navbar';
 import Image from 'next/image';
 import Link from 'next/link';
 import Container from '@/components/Container';
 import { FiArrowUpRight } from "react-icons/fi";
+import { UserContext } from '@/store/features/User/UserContext';
 
 const Hero = () => {
+
+    const { userProfile } = useContext(UserContext);
+
     return (
         <header className='bg-primary lg:h-screen pb-10 relative'>
             <Navbar />
@@ -35,15 +40,22 @@ const Hero = () => {
                             Project management software that enables you and your teams
                             to collate, analyze and manage everyday feedbacks.
 
-                            Seamlessly collect, organize, and share insights across all 
+                            Seamlessly collect, organize, and share insights across all
                             your products with our innovative tool
                         </p>
 
-                        <Link href="/register" className='group cursor-pointer flex items-center space-x-2 w-fit  bg-[#FFE492] rounded-md text-base font-medium py-3 px-10 text-primary '>
-                            <span>Try FeedShare free</span>
+                        {
+                            userProfile ? <Link href="/dashboard" className='group cursor-pointer flex items-center space-x-2 w-fit  bg-[#FFE492] rounded-md text-base font-medium py-3 px-10 text-primary '>
+                                <span>Go to dashboard</span>
 
-                            <FiArrowUpRight className='group-hover:animate-bounce text-xl'/>
-                        </Link>
+                                <FiArrowUpRight className='group-hover:animate-bounce text-xl' />
+                            </Link> :
+                                <Link href="/register" className='group cursor-pointer flex items-center space-x-2 w-fit  bg-[#FFE492] rounded-md text-base font-medium py-3 px-10 text-primary '>
+                                    <span>Try FeedShare free</span>
+
+                                    <FiArrowUpRight className='group-hover:animate-bounce text-xl' />
+                                </Link>
+                        }                       
                     </div>
 
                     <div className='relative lg:mt-0 mt-10 block lg:flex items-center justify-center'>
@@ -51,7 +63,7 @@ const Hero = () => {
                         <Image src="/heroImg2.png" className='block lg:hidden' alt="hero image" height={375} width={500} />
                     </div>
                 </Container>
-            </section>            
+            </section>
         </header>
     )
 }
--- components/widget/landing-page/Navbar.tsx
+++ components/widget/landing-page/Navbar.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Container from '@/components/Container';
 import Image from 'next/image';
 import Link from 'next/link';
 import { HiMiniBars3CenterLeft } from 'react-icons/hi2';
+import { UserContext } from '@/store/features/User/UserContext';
 
 const Navbar = () => {
     const [openNavbar, setOpenNavbar] = useState<boolean>(false)
+
+    const { userProfile } = useContext(UserContext);
+
+    console.log("userProfile", userProfile)
 
     const handleOpenNavbar = () => {
         setOpenNavbar(prev => !prev);
@@ -35,9 +40,17 @@ const Navbar = () => {
                     </ul>
 
                     <div>
-                        <Link href="/login" className='bg-[#FFE492] md:block hidden rounded-md text-base font-medium py-3 px-10 text-primary '>
+                        {
+                            userProfile ? <Link href="/dashboard" className='bg-[#FFE492] md:block hidden rounded-md text-base font-medium py-3 px-10 text-primary '>
+                                Go to dashboard
+                            </Link> :
+                                <Link href="/login" className='bg-[#FFE492] md:block hidden rounded-md text-base font-medium py-3 px-10 text-primary '>
+                                    Login
+                                </Link>
+                        }
+                        {/* <Link href="/login" className='bg-[#FFE492] md:block hidden rounded-md text-base font-medium py-3 px-10 text-primary '>
                             Login
-                        </Link>
+                        </Link> */}
 
                         <div className='md:hidden block'>
                             <HiMiniBars3CenterLeft onClick={handleOpenNavbar} className='cursor-pointer text-white text-2xl' />
@@ -50,11 +63,17 @@ const Navbar = () => {
                     openNavbar && <div className={`absolute transition-all -translate-y-8 ${openNavbar && 'translate-y-0'} top-10 right-0 w-full md:hidden block mt-2 rounded-md px-3 py-6 bg-white text-primary`}>
                         <ul className='space-y-6 text-right'>
                             <li>
-                                <Link onClick={()=> setOpenNavbar(false)} href="#features">Features</Link>
+                                <Link onClick={() => setOpenNavbar(false)} href="#features">Features</Link>
                             </li>
 
                             <li>
-                                <Link onClick={()=> setOpenNavbar(false)} href="#">How it works</Link>
+                                <Link onClick={() => setOpenNavbar(false)} href="#">How it works</Link>
+                            </li>
+
+                            <li>
+                                <Link className='bg-[#FFE492] rounded-md text-base font-medium py-3 px-10 text-primary' onClick={() => setOpenNavbar(false)} href="/login">
+                                    Login
+                                </Link>
                             </li>
                         </ul>
                     </div>
